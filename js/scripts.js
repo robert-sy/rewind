cf0a67041071fefc8ac9b087b6bac169f976532a
paul.sturm <paul.sturm@project-a.com>
2014-08-28 22:54:44 +0200

    added carousel

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(function() {
+
+//    var $tes = $("#tesklick");
+//    $tes.css('background', 'red');
+//    $tes.attr("href", "#furz");
+//    $tes.on('click', function(ev) {
+//
+//        console.log(ev);
+//
+//
+//    });
+
+
+
+
+});
